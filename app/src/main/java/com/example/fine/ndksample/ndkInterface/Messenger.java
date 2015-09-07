5b48b40eb1e570cd57a8fdf91b1124ef535400bd
fine1021 <1078041387@qq.com>
2015-09-07 22:53:22 +0800

    rename Messager to Messenger, add synchronized lock

---
+++ app/src/main/java/com/example/fine/ndksample/ndkInterface/Messenger.java
@@ -0,0 +1,44 @@
+package com.example.fine.ndksample.ndkInterface;
+
+import java.util.ArrayList;
+
+/**
+ * Messenger
+ */
+@SuppressWarnings("ALL")
+public class Messenger {
+
+    /**
+     * Lock used to modify the content of {@link #sCallBacks}. Any write operation
+     * performed on the array should be synchronized on this lock.
+     */
+    private static final Object mLock = new Object();
+
+    private static ArrayList<MessageCallBack> sCallBacks = new ArrayList<>();
+
+    public static void addListener(MessageCallBack callBack) {
+        synchronized (mLock) {
+            if (!sCallBacks.contains(callBack)) {
+                sCallBacks.add(callBack);
+            }
+        }
+    }
+
+    public static void removeListener(MessageCallBack callBack) {
+        synchronized (mLock) {
+            if (sCallBacks.contains(callBack)) {
+                sCallBacks.remove(callBack);
+            }
+        }
+    }
+
+    public void notifyMessage(String msg) {
+        for (MessageCallBack callBack : sCallBacks) {
+            callBack.onCallBack(msg);
+        }
+    }
+
+    public interface MessageCallBack {
+        void onCallBack(String msg);
+    }
+}
